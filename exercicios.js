849262ff2267ff21ba9ae4f6a8b71a8c85ebd72c
Juliocesar1303 <142456148+Juliocesar1303@users.noreply.github.com>
2023-08-22 21:49:58 -0300

    Create exercicios.js

---
+++ exercicios.js
@@ -0,0 +1,10 @@
+/*
+* Julio
+* 
+*/
+
+/*1 resposta
+no primeiro "console.log"(b)" impresso no valor de b, "10" , depois o valor foi alterado para 5. logo no segundo sera impresso a,b , 10,5
+*
+/*2 resposta
+* diz qui o valor de c é igual de a , o valor de b = c, e o valor 
